beccf9d2456272c53e212c0e93f4810eae5065c6
charie411 <30856597+charie411@users.noreply.github.com>
2019-02-11 17:12:57 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,108 +1,91 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.ensemble import RandomForestClassifier
-from sklean.metrics import roc_auc_score, roc_curve, auc
+from sklean.metrics import roc_auc_score, roc_curve, auc, confusion_matrix
 from sklearn import svm, preprocessing
 from sklearn.feature_selection import SelectKBest, chi2
 import pickle, os
 import matplotlib.pyplot as plt
 
-#Define a function to distinguish positive from negative instances
-def sortClass(feature, label):
-  samp_feat_p, samp_feat_n = [], []
-  
-  for smp,lbl in zip(feature,label):
-  if lbl == 1:
-    samp_feat_p.append(smp)
-  elif lbl == 0:
-    dsmp_feat_n.append(smp)
-  return samp_feat_p, samp_feat_n
-  
 #Define a function to train Random Forest Model
-def RfModelDecision(train_x, label, Ptest_x, Ntest_x):
+def RfModelDecision(train_x, train_y, test_x):
   rf_model = RandomForestClassifier(n_estimators=60)
-  rf_model.fit(train_x,label)
+  rf_model.fit(train_x,train_y)
   
-  Ppred_rf = rf_model.predict_proba(Ptest_x)
-  Npred_rf = rf_model.predict_proba(Ntest_x)
+  test_pred_proba_rf = rf_model.predict_proba(test_x)
   
-  test_label = [1] * len(Ppred_rf) + [0] * len(Npred_rf)
-  score = Ppred_rf.tolist() +Npred_rf.tolist()
-  score_rf = [x[rf_model.classes_.argmax()] for x in score]
-  ROC_Plot(score_rf, test_label)
-  auc = roc_auc_score(test_label, score_rf)
-  print('rf_AUC:', auc)
+  score = test_pred_proba_rf.tolist()
+  score_rf = [x[1] for x in score]
   
-  return score_rf, rf_model, test_label
+  return score_rf, rf_model
   
 #Define a function to train SVM Model
-def SVMModelDecision(train_x, label, Ptest_x, Ntest_x):
+def SVMModelDecision(train_x, train_y, test_x):
   svm_model = svm.SVC(C=35, gamma=0.005, kernel='rbf', probability=True)
-  svm_model.fit(train_x,label)
+  svm_model.fit(train_x,train_y)
   
-  Ppred_svm = svm_model.predict_proba(Ptest_x)
-  Npred_svm = svm_model.predict_proba(Ntest_x)
+  test_pred_proba_svm = svm_model.predict_proba(test_x)
   
-  test_label = [1] * len(Ppred_svm) + [0] * len(Npred_svm)
-  score = Ppred_svm,f.tolist() +Npred_svm.tolist()
-  score_svm = [x[svm_model.classes_.argmax()] for x in score]
-  ROC_Plot(score_svm, test_label)
-  auc = roc_auc_score(test_label, score_svm)
-  print('rf_AUC:', auc)
+  score = test_pred_proba_svm.tolist() 
+  score_svm = [x[1] for x in score]
   
-  return score_svm, svm_model, test_label
+  return score_svm, svm_model
   
 #Define a function to print and plot roc and auc info 
 def ROC_Plot(predict_vec, label):
   fpr, tpr, thresholds = roc_curve(label, predict_vec)
-  roc_auc = suc(fpr, tpr)
-  print('AUC:',auc)
+  roc_auc = auc(fpr, tpr)
+  print('AUC:',roc_auc)
   plt.plot(fpr,tpr,lw=1)
 
 #Define a function to Kfold validate models  
-def kf_model(X_p, X_n, clf, models=[], scores=[]):
-  folds =KFold(n_splits=5, random_state=2019)
+def stratifiedkf_model(feature, label, clf, models=[], scores=[]):
+  folds = StratifiedKFold(n_splits=5, random_state=2019)
   
-  for fold_, (trn_idx,val_idx) in enumerate(folds.split(X_p,X_n)):
+  for fold_, (trn_idx,val_idx) in enumerate(folds.split(feature,label)):
     print('fold n°{}'.format(fold_+1))
     
-    X_p_train, X_p_val = X_p[trn_idx], X_p[cal_idx]
-    X_n_train, X_n_val = X_n[trn_idx], X_n[cal_idx]
-    train_x = X_p_train + X_n_train
-    train_y = [1] * len(X_p_train) + [0] * len(y_n_train)
-    
-    tmp_score, model, test_label = clf(train_x, train_y, X_p_val, X_n_val)
+    train_x, val_x = feature[trn_idx], feature[cal_idx]
+    train_y, val_y = label[trn_idx], label[cal_idx]
+       
+    tmp_score, model = clf(train_x, train_y, val_x)
     models.append(model)
     scores.append(tmp_score)
     
     #check training set accuracy
-    sum_Ptrain = sum(model.predict(X_p_train))
-    sum_Ntrain = len(X_n_train) -sum(model.predict(X_n_train))
-    print('training set accuracy ammounts: P:%s N:%s'%(sum_Ptrain, sum_Ntrain))
-  return models, scores, test_label
+    train_pred = model.predict(train_x)
+    tn, fp, fn, tp = confusion_matrix(y_pred=train_pred, y_true=train_y).ravel()
+    print('training set accuracy ammounts: P:%s N:%s'%(tp, tn))
+    
+    #check validate set accuracy
+    ROC_Plot(tmp_score, val_y)
+  return models, scores, val_y
   
 #Define a function to train, fit, submit results
 def ModelMainFunc(operation):
   if operation == 'train':
-    feature_all = pickle.load(open("./data/feature_train/feat_final.pytmp"))
+    train_feat_org = pickle.load(open("./data/feature_train/feat_final.pytmp"))
     label = pickle.load(open("./data/feature_train/label.pytmp"))
-    scaler_file = "./data/model/scaler.pytmp"
+#     scaler_file = "./data/model/scaler.pytmp"
 
-    if os.path.exists(scaler_file):
-      scaler = preprocessing.StandardScaler().fit(feature_all)
-      pickle.dump(scaler, open(scaler_file, 'wb'))
-    else:
-      scaler = pickle.load(open(scaler_file, 'wb'))
+#     if os.path.exists(scaler_file):
+#       scaler = preprocessing.StandardScaler().fit(feature_all)
+#       pickle.dump(scaler, open(scaler_file, 'wb'))
+#     else:
+#       scaler = pickle.load(open(scaler_file, 'wb'))
 
-    feat_scale = scaler.transform(feature_all)
+#     feat_scale = scaler.transform(feature_all)
 
-    feat_scale_new = SelectKBest(chi2,k=23).fit_transform(feat_scale, label)
-    X_p, X_n = sortClass(feat_scale_new, label)
+    feat_scale_new = SelectKBest(chi2,k=23).fit(train_feat_org, label)
+    train_feat_new = selectkbest.transform(train_feat_org)
+    pickle.dump(selectkbest, open("./data/model/selectkbest.pytmp", 'wb'))
 
     #SVM model
     print('*' * 50)
     print('Start Building SVM model...')
-    svm_models, svm_scores, test_label = kf_model(X_p, X_n, clf=SVMModelDecision, models=[], scores=[])
+    label = np.array(label)
+    svm_models, svm_scores, test_label = stratifiedkf_model(train_feat_new, label, clf=SVMModelDecision, models=[], scores=[])
 
     svm_model_file = "./data/model/svm_model_files.pytmp"
     pickle.dump(obj=svm_models, file=open(svm_model_file, 'wb'))
@@ -110,7 +93,7 @@ def ModelMainFunc(operation):
     #Random Forest Model
     print('*' * 50)
     print('Start Building Random Forest model...')
-    rf_models, rf_scores, test_label = kf_model(X_p, X_n, clf=RfModelDecision, models=[], scores=[])
+    rf_models, rf_scores, test_label = stratifiedkf_model(train_feat_new, label, clf=RfModelDecision, models=[], scores=[])
 
     rf_model_file = "./data/model/rf_model_files.pytmp"
     pickle.dump(obj=rf_models, file=open(rf_model_file, 'wb')) 
@@ -118,10 +101,8 @@ def ModelMainFunc(operation):
     svm_scores_arr = np.array(svm_scores)
     rf_scores_arr = np.array(rf_scores)
     score_integ = (0.5 * (svm_scores_arr + rf_scores_arr)).tolist()
-    auc = roc_auc_score(test_label, score_integ)
     for scr in score_integ:
       ROC_Plot(scr, test_label)
-    print('total_AUC:', auc)
 
   if operation == 'test':
     test_feat_org = pickle.load(open("./data/feature/feat_final.pytmp",'rb'))
